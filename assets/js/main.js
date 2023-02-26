ad1c7643307d0f36861a079b23b28977c8ad6787
samandarrza <samandarshr@code.edu.az>
2023-02-26 23:46:00 +0400

    add product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,48 @@ $('.main_slider').slick({
     ]
   });
 
+  $('.review-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 200000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
   $('.product-one').slick({
     dots: false,
     infinite: true,
