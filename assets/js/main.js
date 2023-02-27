32d7f6b31db6d23ec9a3d17c67ec3975d2505101
samandarrza <samandarshr@code.edu.az>
2023-02-27 17:01:51 +0400

    change product

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,5 +158,42 @@ $('.main_slider').slick({
     ]
   });
 
+  $('.product-small-thumb').slick({
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    focusOnSelect: true,
+    vertical: true,
+    speed: 800,
+    asNavFor: '.product-large-thumbnail',
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                vertical: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                vertical: false,
+                slidesToShow: 4,
+            }
+        }
+    ]
+
+});
+
+$('.product-large-thumbnail').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    speed: 800,
+    draggable: false,
+    asNavFor: '.product-small-thumb'
+});
 
   
