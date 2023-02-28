b4946010a1ccd9347b032d561c05c064f3247d92
samandarrza <samandarshr@code.edu.az>
2023-02-28 21:02:40 +0400

    add contant page

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,4 +174,8 @@ $('.main_slider').slick({
     focusOnSelect: true
   });
 
-  
+  hamburger = document.querySelector(".mobile");
+    hamburger.onclick = function () {
+        navbar = document.querySelector(".mobile-menu");
+        navbar.classList.toggle("active");
+    }
